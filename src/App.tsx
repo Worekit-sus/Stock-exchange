c6f93c2739e61ca03fe8de5520d9eb196459e953
Worekit <worekit@gmail.com>
2023-03-30 19:17:44 +0200

    start

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  // OG9LS4THFHZYBROP
+  // dane spółki, symbol 
+  // API - alpha vantage
+  // wykres do zmiany cen dzisiaj, tydzien, miesiąc, rok - biblioteka highcharts wrapper
+  // oraz typ wykresu liniowy, słupkowy, ...
+  // lista ulubionych spółek
+  
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
