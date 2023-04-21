ee9f9b868b4119fdc66a251e86b38abc5824b9e3
Worekit <worekit@gmail.com>
2023-04-21 13:55:03 +0200

    mnóstwo rzeczy do zrobienia

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   // dane spółki, symbol 
   // API - alpha vantage
   // wykres do zmiany cen dzisiaj, tydzien, miesiąc, rok - biblioteka highcharts wrapper
-  // oraz typ wykresu liniowy, słupkowy, ...
+  // oraz typ wykresu line, area, candlestick, sparkliness
   // lista ulubionych spółek
   
   return (
@@ -15,12 +15,39 @@ function App() {
       <div className='main-container'>
         <div className='header'>
           <h1>Stock exchange</h1>
+          
+            <h1>BTC +0,06%</h1>
+            <h1>ETH +0,32%</h1>
+            <h1>BNB -0,07%</h1>
+          
+          <select>
+            <option>USD $</option>
+            <option>EUR €</option>
+            <option>PLN zł</option>
+            <option>JPY ¥</option>
+            <option>GBP £</option>
+            <option>CAD $</option>
+            <option>ARS $</option>
+          </select>
         </div>
         <div className='favorite-search'>
-          <h1>Favorite stocks and search</h1>
+          
+
+          <div>
+            <h1>Stocks you follow</h1>
+            <h3>Bitcoin</h3>
+            <h3>Ethereum</h3>
+            <h1>Search stocks</h1>
+
+            <input type="text" placeholder="Search stocks"  />
+            <h3>Dogecoin</h3>
+            <h3>Polygon</h3>
+          </div>
         </div>
         <div className='stock-data'>
           <h1>Stock data</h1>
+          <h1>img BTC +0.60%</h1>
+          <h4>1 BTC =        126.2149534 PLN</h4>
         </div>
       </div>
     </>
