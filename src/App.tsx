4dac700f1f1c3522bb39e1643a8fdb63d0dbf9e5
Worekit <worekit@gmail.com>
2023-05-24 23:16:28 +0200

    c

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-
+import {StockData} from './Types';
 
 const Lista: {time: number, open: number,high: number,low: number, close: number}[] = [];
 
@@ -12,82 +12,29 @@ function App() {
   // wykres do zmiany cen dzisiaj, tydzien, miesiąc, rok - biblioteka highcharts wrapper
   // oraz typ wykresu line, area, candlestick, sparkliness
   // lista ulubionych spółek
-  [
-    "btc",
-    "eth",
-    "ltc",
-    "bch",
-    "bnb",
-    "eos",
-    "xrp",
-    "xlm",
-    "link",
-    "dot",
-    "yfi",
-    "usd",
-    "aed",
-    "ars",
-    "aud",
-    "bdt",
-    "bhd",
-    "bmd",
-    "brl",
-    "cad",
-    "chf",
-    "clp",
-    "cny",
-    "czk",
-    "dkk",
-    "eur",
-    "gbp",
-    "hkd",
-    "huf",
-    "idr",
-    "ils",
-    "inr",
-    "jpy",
-    "krw",
-    "kwd",
-    "lkr",
-    "mmk",
-    "mxn",
-    "myr",
-    "ngn",
-    "nok",
-    "nzd",
-    "php",
-    "pkr",
-    "pln",
-    "rub",
-    "sar",
-    "sek",
-    "sgd",
-    "thb",
-    "try",
-    "twd",
-    "uah",
-    "vef",
-    "vnd",
-    "zar",
-    "xdr",
-    "xag",
-    "xau",
-    "bits",
-    "sats"
-  ]
+  
+  //1594382400000 (time),
+  // 1.1 (open),
+  // 2.2 (high),
+  // 3.3 (low),
+  // 4.4 (close)
 
   const [stock, setStock] = useState("bitcoin");
   const [currency, setCurrency] = useState("usd");
-  const [days, setDays] = useState("1");
-  const [data, setData] = useState(Lista);
-  const [list, setList] = useState(Lista);
+  const [days, setDays] = useState("30");
+  const [data, setData] = useState<StockData>();
+
+
+  const FetchData = () => {
+    fetch(`https://api.coingecko.com/api/v3/coins/${stock}/ohlc?vs_currency=${currency}&days=${days}`)
+    .then(response => response.json())
+    .then(data => setData(data))  
+  }
 
   // przyp;isać do jakiejś list z obiektów czy coś no i charciki oraz autocompleta z currensami
   useEffect(() => {
-    fetch(`https://api.coingecko.com/api/v3/coins/${stock}/ohlc?vs_currency=${currency}&days=${days}`)
-    .then(response => response.json())
-    .then(data => setData(data))
-    .catch(error => console.log(error));
+    FetchData();
+   
   },[])
 
   console.log(data);
@@ -97,9 +44,7 @@ function App() {
         <div className='header'>
           <h1>Stock exchange</h1>
           
-            <h1>BTC +0,06%</h1>
-            <h1>ETH +0,32%</h1>
-            <h1>BNB -0,07%</h1>
+            
           
           <select>
             <option>USD $</option>
@@ -116,18 +61,16 @@ function App() {
 
           <div>
             <h1>Stocks you follow</h1>
-            <h3>Bitcoin</h3>
-            <h3>Ethereum</h3>
+            
             <h1>Search stocks</h1>
 
-            <input type="text" placeholder="Search stocks"  />
-            <h3>Dogecoin</h3>
-            <h3>Polygon</h3>
+            <input type="text" placeholder="Search stocks" value={stock} onChange={(e) => setStock(e.target.value)}  />
+            
           </div>
         </div>
         <div className='stock-data'>
 
-          
+          {stock}
 
         </div>
       </div>
